5a9b2e743324a67adfb27bdc160e28250497df17
Shaimaa Elshafei <pro.shaimaa@gmail.com>
2020-04-22 12:34:21 +0300

    activity 4 is finished

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,12 +1,19 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextBtn(view : View){
+        val nextIntent = Intent(this,SkillActivity::class.java)
+        startActivity(nextIntent)
     }
 }
